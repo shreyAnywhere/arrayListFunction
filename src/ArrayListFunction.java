3ef503d03e4e80b8d47d5e461f93cadf9b30ab63
Shreypatel911 <shreypatel11999@gmail.com>
2022-05-04 16:49:15 +0530

    add method implemented

--- src/ArrayListFunction.java
+++ src/ArrayListFunction.java
@@ -1,5 +1,31 @@
-
 public class ArrayListFunction {
 
+    int[] arr;
+    int currSize = 0;
+    int totalSize = 1;
 
+    public ArrayListFunction(){
+        arr = new int[1];
+    }
+
+    public void increaseSize(){
+        int[] biggerArr = new int[2 * arr.length];
+
+        for(int i=0;i<currSize;i++)
+            biggerArr[i] = arr[i];
+        arr = biggerArr;
+        totalSize = totalSize * 2;
+    }
+
+    public void add(int e){
+        if(currSize == totalSize)
+            increaseSize();
+
+        arr[currSize++] = e;
+    }
+
+    public void printArray() {
+        for(int i=0;i<currSize;i++)
+            System.out.print(arr[i] + " ");
+    }
 }
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayListFunction obj = new ArrayListFunction();
+
+        obj.add(5);
+        obj.add(6);
+        obj.add(10);
+
+        obj.printArray();
+    }
+}
